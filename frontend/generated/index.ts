2c72cd368dd8a8e2912cc5938a912b70764471be
Rashad Philizaire <rashadphilizaire@gmail.com>
2024-04-28 15:29:10 -0500

    added generated code

---
+++ frontend/generated/index.ts
@@ -0,0 +1,16 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export { ApiError } from './core/ApiError';
+export { CancelablePromise, CancelError } from './core/CancelablePromise';
+export { OpenAPI } from './core/OpenAPI';
+export type { OpenAPIConfig } from './core/OpenAPI';
+
+export type { ChatRequest } from './models/ChatRequest';
+export type { HTTPValidationError } from './models/HTTPValidationError';
+export type { Message } from './models/Message';
+export { MessageRole } from './models/MessageRole';
+export type { ValidationError } from './models/ValidationError';
+
+export { DefaultService } from './services/DefaultService';
---
+++ frontend/generated/models/HTTPValidationError.ts
@@ -0,0 +1,9 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+import type { ValidationError } from './ValidationError';
+export type HTTPValidationError = {
+    detail?: Array<ValidationError>;
+};
+
