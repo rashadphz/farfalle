4890a2e5e7497ea388850854b36a17b4abcb95f1
Rashad Philizaire <rashadphilizaire@gmail.com>
2024-04-29 03:37:18 -0500

    lovely citations

--- frontend/src/components/message.tsx
+++ frontend/src/components/message.tsx
@@ -1,6 +1,7 @@
-import React, { FC, memo } from "react";
+import React, { FC, memo, useMemo } from "react";
 import { MemoizedReactMarkdown } from "./markdown";
 import _ from "lodash";
+import { cn } from "@/lib/utils";
 
 function chunkString(str: string): string[] {
   const words = str.split(" ");
@@ -13,20 +14,95 @@ export interface MessageProps {
   isStreaming?: boolean;
 }
 
-// Fix for the animation delay
-const MemoizedParagraph = memo(
-  ({ children }: React.HTMLProps<HTMLParagraphElement>) => (
-    <p>
-      {chunkString(children?.toString() || "").map((chunk, index) => (
-        <span key={index} className="animate-in fade-in-25 duration-700">
-          {chunk}
+const Citation = memo(({ children }: React.HTMLProps<HTMLParagraphElement>) => {
+  return (
+    <a className="ml-1">
+      <span className="relative -top-[0.2rem] inline-flex">
+        <span className="h-[1rem] min-w-[1rem] items-center justify-center rounded-full  text-center px-1 text-xs font-mono bg-muted text-[0.60rem] ">
+          {children}
         </span>
-      ))}
-    </p>
-  )
+      </span>
+    </a>
+  );
+});
+
+const TextWithCitations = ({
+  children,
+  isStreaming,
+  containerElement = "p",
+}: {
+  children: React.ReactNode;
+  isStreaming: boolean;
+  containerElement: React.ElementType;
+}) => {
+  const citationMatch = /(\[\d+\])/g;
+  const chunks = chunkString(children?.toString() || "");
+  const textWithCitations = isStreaming
+    ? chunks.flatMap((chunk) => chunk.split(citationMatch))
+    : children?.toString().split(citationMatch);
+
+  const text = textWithCitations?.map((text, index) => {
+    if (text.match(citationMatch)) {
+      const number = text.slice(1, -1);
+      return <Citation key={number}>{number}</Citation>;
+    }
+    return (
+      <span
+        key={`${index}-streaming`}
+        className={cn(isStreaming ? "animate-in fade-in-25 duration-700" : "")}
+      >
+        {text}
+      </span>
+    );
+  });
+
+  console.log({ textWithCitations: children });
+
+  return React.createElement(containerElement, {}, text);
+};
+
+const StreamingParagraph = memo(
+  ({ children }: React.HTMLProps<HTMLParagraphElement>) => {
+    return (
+      <TextWithCitations isStreaming={true} containerElement="p">
+        {children}
+      </TextWithCitations>
+    );
+  }
+);
+const Paragraph = memo(
+  ({ children }: React.HTMLProps<HTMLParagraphElement>) => {
+    return (
+      <TextWithCitations isStreaming={false} containerElement="p">
+        {children}
+      </TextWithCitations>
+    );
+  }
 );
 
-MemoizedParagraph.displayName = "MemoizedParagraph";
+const ListItem = memo(({ children }: React.HTMLProps<HTMLLIElement>) => {
+  return (
+    <TextWithCitations isStreaming={false} containerElement="li">
+      {children}
+    </TextWithCitations>
+  );
+});
+
+const StreamingListItem = memo(
+  ({ children }: React.HTMLProps<HTMLLIElement>) => {
+    return (
+      <TextWithCitations isStreaming={true} containerElement="li">
+        {children}
+      </TextWithCitations>
+    );
+  }
+);
+
+StreamingParagraph.displayName = "StreamingParagraph";
+Paragraph.displayName = "Paragraph";
+Citation.displayName = "Citation";
+ListItem.displayName = "ListItem";
+StreamingListItem.displayName = "StreamingListItem";
 
 export const MessageComponent: FC<MessageProps> = ({
   message,
@@ -35,8 +111,11 @@ export const MessageComponent: FC<MessageProps> = ({
   return (
     <MemoizedReactMarkdown
       components={{
+        // TODO: For some reason, can't pass props into the components
         // @ts-ignore
-        p: isStreaming ? MemoizedParagraph : "p",
+        p: isStreaming ? StreamingParagraph : Paragraph,
+        // @ts-ignore
+        li: isStreaming ? StreamingListItem : ListItem,
       }}
       className="prose dark:prose-invert inline leading-relaxed break-words "
     >
