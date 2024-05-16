20dff6691e3072a253a09ad5defde30de8717094
Rashad Philizaire <rashadphilizaire@gmail.com>
2024-05-16 03:05:46 -0400

    added discord

--- src/frontend/src/components/footer.tsx
+++ src/frontend/src/components/footer.tsx
@@ -1,11 +1,16 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
-import { SiX, SiGithub } from "react-icons/si";
+import { SiX, SiGithub, SiDiscord } from "react-icons/si";
 
 export function Footer() {
   return (
     <footer className="w-full flex fixed bottom-0 right-0 p-1 z-50 bg-background/95">
       <div className="px-1 w-full flex flex-row justify-end space-x-1">
+        <Button variant="ghost" size="icon" className="hover:bg-transparent">
+          <Link href="https://discord.gg/kKmmqmjx" target="_blank">
+            <SiDiscord size={16} />
+          </Link>
+        </Button>
         <Button variant="ghost" size="icon" className="hover:bg-transparent">
           <Link href="https://git.new/farfalle" target="_blank">
             <SiGithub size={16} />
