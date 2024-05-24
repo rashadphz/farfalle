77e1d8361fdce418e673a964c0490555260b2324
Rashad Philizaire <rashadphilizaire@gmail.com>
2024-05-24 18:18:21 -0400

    added suspense

--- src/frontend/src/components/chat-panel.tsx
+++ src/frontend/src/components/chat-panel.tsx
@@ -4,7 +4,7 @@ import { useParams, useSearchParams } from "next/navigation";
 import { useChat } from "@/hooks/chat";
 import { useMessageStore } from "@/stores";
 import { MessageType } from "@/types";
-import { useCallback, useEffect, useRef, useState } from "react";
+import { Suspense, useCallback, useEffect, useRef, useState } from "react";
 import { AskInput } from "./ask-input";
 
 import MessagesList from "./messages-list";
@@ -75,38 +75,38 @@ export const ChatPanel = () => {
     }
   }, [queryMessage]);
 
-  if (messages.length > 0) {
-    return (
-      <div ref={messagesRef} className="pt-10 w-full relative">
-        <MessagesList
-          messages={messages}
-          streamingMessage={streamingMessage}
-          onRelatedQuestionSelect={handleSend}
-        />
-        <div ref={messageBottomRef} className="h-0" />
-        <div
-          className="bottom-12 fixed px-2 max-w-screen-md justify-center items-center md:px-2"
-          style={{ width: `${width}px` }}
-        >
-          <AskInput isFollowingUp sendMessage={handleSend} />
-        </div>
-      </div>
-    );
-  } else {
-    return (
-      <div className="w-full flex flex-col justify-center items-center">
-        <div className="flex items-center justify-center mb-8">
-          <span className="text-3xl">Ask anything</span>
-        </div>
-        <AskInput sendMessage={handleSend} />
-        <div className="w-full flex flex-row px-3 justify-between space-y-2 pt-1">
-          <StarterQuestionsList handleSend={handleSend} />
-          <div className="flex flex-col gap-2 items-end ">
-            <ModelSelection />
-            <LocalToggle />
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      {messages.length > 0 ? (
+        <div ref={messagesRef} className="pt-10 w-full relative">
+          <MessagesList
+            messages={messages}
+            streamingMessage={streamingMessage}
+            onRelatedQuestionSelect={handleSend}
+          />
+          <div ref={messageBottomRef} className="h-0" />
+          <div
+            className="bottom-12 fixed px-2 max-w-screen-md justify-center items-center md:px-2"
+            style={{ width: `${width}px` }}
+          >
+            <AskInput isFollowingUp sendMessage={handleSend} />
           </div>
         </div>
-      </div>
-    );
-  }
+      ) : (
+        <div className="w-full flex flex-col justify-center items-center">
+          <div className="flex items-center justify-center mb-8">
+            <span className="text-3xl">Ask anything</span>
+          </div>
+          <AskInput sendMessage={handleSend} />
+          <div className="w-full flex flex-row px-3 justify-between space-y-2 pt-1">
+            <StarterQuestionsList handleSend={handleSend} />
+            <div className="flex flex-col gap-2 items-end ">
+              <ModelSelection />
+              <LocalToggle />
+            </div>
+          </div>
+        </div>
+      )}
+    </Suspense>
+  );
 };
