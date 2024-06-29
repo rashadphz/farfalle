b54d7cc6f74ccf283e1ba2ea51fb26ac3d8f69b3
Rashad Philizaire <rashadphilizaire@gmail.com>
2024-06-29 16:15:13 -0400

    fixes

--- src/frontend/src/env.mjs
+++ src/frontend/src/env.mjs
@@ -7,14 +7,14 @@ export const env = createEnv({
   server: {},
   client: {
     NEXT_PUBLIC_API_URL: z.string(),
-    NEXT_PUBLIC_LOCAL_MODE_ENABLED: z.boolean().default(false),
-    NEXT_PUBLIC_PRO_MODE_ENABLED: z.boolean().default(false),
+    NEXT_PUBLIC_LOCAL_MODE_ENABLED: z.boolean().default(true),
+    NEXT_PUBLIC_PRO_MODE_ENABLED: z.boolean().default(true),
   },
   runtimeEnv: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
     NEXT_PUBLIC_LOCAL_MODE_ENABLED:
-      process.env.NEXT_PUBLIC_LOCAL_MODE_ENABLED === "true",
+      process.env.NEXT_PUBLIC_LOCAL_MODE_ENABLED.toLowerCase() === "true",
     NEXT_PUBLIC_PRO_MODE_ENABLED:
-      process.env.NEXT_PUBLIC_PRO_MODE_ENABLED === "true",
+      process.env.NEXT_PUBLIC_PRO_MODE_ENABLED.toLowerCase() === "true",
   },
 });
